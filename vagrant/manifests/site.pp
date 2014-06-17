4251f8f880c7bfc0dcf3ee7c4dce35a6a2b8cafe
Kevin Rogers <kev@orican.eu>
2014-06-17 17:37:26 +0100

    Add PHP/PEAR/XDEBUG to vagrant configuration

--- vagrant/manifests/site.pp
+++ vagrant/manifests/site.pp
@@ -9,3 +9,22 @@ apache::vhost { 'substance.vm':
   docroot => '/vagrant/public',
   serveradmin => 'admin@substance.vm',
 }
+
+package { 'php-devel':
+}
+
+package { 'php-pear':
+}
+
+exec { 'pear-xdebug':
+  command => '/usr/bin/pecl install xdebug',
+  creates => '/usr/lib/php/modules/xdebug.so',
+  require => Package['php-devel', 'php-pear'],
+}
+
+file { 'php-xdebug-ini':
+  path => '/etc/php.d/xdebug.ini',
+  ensure => 'present',
+  mode => 0644,
+  content => 'zend_extension="/usr/lib/php/modules/xdebug.so"',
+}
