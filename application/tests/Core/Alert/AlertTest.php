98604a6e4035a6074161845e6531cc35216abb64
Kevin Rogers <kev@orican.eu>
2014-11-20 14:52:53 +0000

    Add a crude HTTP environment Alert test

--- application/tests/Core/Alert/AlertTest.php
+++ application/tests/Core/Alert/AlertTest.php
@@ -1,20 +1,34 @@
 <?php
 use Substance\Core\Alert\Alert;
 use Substance\Core\Environment\CliEnvironment;
+use Substance\Core\Environment\HttpEnvironment;
 
 class AlertTest extends PHPUnit_Framework_TestCase {
 
   /**
-   * Crude test of basic Alert functionality.
+   * Crude test of basic Alert functionality in a CLI environment.
    */
-  public function testAlert() {
+  public function testCliAlert() {
     CliEnvironment::initialise();
     $alert = Alert::alert('ahhhh')->culprit( 'who', 'me' );
     $alert = $alert->__toString();
 
-    $this->assertContains( 'Message : ahhh', $alert );
-    $this->assertRegExp( '#Origin : .*?tests/Core/Alert/AlertTest.php#', $alert );
+    $this->assertContains( 'Message : ahhhh', $alert );
+    $this->assertRegExp( '#Origin : .*?tests/Core/Alert/AlertTest.php\(\d+\)#', $alert );
     $this->assertContains( 'WHO : me', $alert );
+  }
+
+  /**
+   * Crude test of basic Alert functionality in an HTTP environment.
+   */
+  public function testHttpAlert() {
+    HttpEnvironment::initialise();
+    $alert = Alert::alert('ahhhh')->culprit( 'who', 'me' );
+    $alert = $alert->__toString();
+
+    $this->assertContains( '<tr><td>Message</td><td>ahhhh</td></tr>', $alert );
+    $this->assertRegExp( '#<tr><td>Origin</td><td>.*?tests/Core/Alert/AlertTest.php\(\d+\)</td></tr>#', $alert );
+    $this->assertContains( '<tr><td>WHO</td><td>me</td></tr>', $alert );
   }
 
 }
