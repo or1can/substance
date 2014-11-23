f81f8921b380d04b7f9f7cdd8e556f2ca04642ec
Kevin Rogers <kev@orican.eu>
2014-11-23 22:54:25 +0000

    Put tests into namespaces matching the classes they are testing.

--- application/tests/Core/Database/Queries/SelectTest.php
+++ application/tests/Core/Database/Queries/SelectTest.php
@@ -1,4 +1,23 @@
 <?php
+/* Substance - Content Management System and application framework.
+ * Copyright (C) 2014 Kevin Rogers
+ *
+ * Substance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Substance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Substance.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Substance\Core\Database\Queries;
+
 use Substance\Core\Alert\Alert;
 use Substance\Core\Database\Drivers\MySQL\Connection;
 use Substance\Core\Database\Expressions\AllColumnsExpression;
@@ -14,7 +33,7 @@ class SelectTestConnection extends Connection {
 
 }
 
-class SelectTest extends PHPUnit_Framework_TestCase {
+class SelectTest extends \PHPUnit_Framework_TestCase {
 
   /**
    * Check the expected theme in a CLI environment.
