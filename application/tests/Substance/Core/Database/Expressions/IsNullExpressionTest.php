74227251f2df77634aebb53f162ce375a409ab59
Kevin Rogers <kev@orican.eu>
2015-01-21 17:14:47 +0000

    Implement the IS NULL expression

---
+++ application/tests/Substance/Core/Database/Expressions/IsNullExpressionTest.php
@@ -0,0 +1,74 @@
+<?php
+/* Substance - Content Management System and application framework.
+ * Copyright (C) 2015 Kevin Rogers
+ *
+ * Substance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Substance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Substance.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Substance\Core\Database\Expressions;
+
+use Substance\Core\Database\TestDatabase;
+
+/**
+ * Tests the is null expression.
+ */
+class IsNullExpressionTest extends \PHPUnit_Framework_TestCase {
+
+  protected $connection;
+
+  /* (non-PHPdoc)
+   * @see PHPUnit_Framework_TestCase::setUp()
+   */
+  public function setUp() {
+    $this->connection = new TestDatabase();
+  }
+
+  /**
+   * Test constructing with an illegal not argument.
+   *
+   * @expectedException Substance\Core\Alert\Alert
+   */
+  public function testConstructIllegalNotArgument() {
+    $left = new ColumnExpression('column1');
+    // not must be a boolean
+    $expr = new IsNullExpression( $left, 5 );
+  }
+
+  /**
+   * Test an is null expression.
+   */
+  public function testBuildIsNull() {
+    $left = new ColumnExpression('column1');
+    // Test building with the default $not.
+    $expr = new IsNullExpression( $left );
+    $sql = $expr->build( $this->connection );
+    $this->assertEquals( '`column1` IS NULL', $sql );
+
+    // Test building with the specified $not.
+    $expr = new IsNullExpression( $left, FALSE );
+    $sql = $expr->build( $this->connection );
+    $this->assertEquals( '`column1` IS NULL', $sql );
+  }
+
+  /**
+   * Test an is not null expression.
+   */
+  public function testBuildIsNotNull() {
+    $left = new ColumnExpression('column1');
+    $expr = new IsNullExpression( $left, TRUE );
+    $sql = $expr->build( $this->connection );
+    $this->assertEquals( '`column1` IS NOT NULL', $sql );
+  }
+
+}
--- public/Substance/Core/Database/Expressions/IsNullExpression.php
+++ public/Substance/Core/Database/Expressions/IsNullExpression.php
@@ -1,6 +1,6 @@
 <?php
 /* Substance - Content Management System and application framework.
- * Copyright (C) 2014 Kevin Rogers
+ * Copyright (C) 2014 - 2015 Kevin Rogers
 *
 * Substance is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -18,15 +18,40 @@
 
 namespace Substance\Core\Database\Expressions;
 
-use Substance\Core\Condition;
+use Substance\Core\Alert\Alert;
+use Substance\Core\Database\Expression;
 
 /**
  * Represents a NULL test condition in a database query.
  *
- * SELECT * FROM table WHERE table.column IS NULL
- * SELECT * FROM table WHERE table.column IS NOT NULL
+ * e.g.the
+ *     table.column1 IS NULL
+ * and
+ *     table column2 IS NOT NULL
+ * part of:
+ *     SELECT * FROM table WHERE table.column1 IS NULL AND table.column2 IS NOT NULL
  */
-class NullCondition implements Condition {
+class IsNullExpression extends AbstractPostfixExpression {
+
+  protected $not;
+
+  /**
+   * Constructs a new null test expression.
+   *
+   * @param Expression $left the expression to perform a null test on.
+   * @param boolean $not FALSE for an 'IS NULL' test or TRUE for an
+   * 'IS NOT NULL' test.
+   */
+  public function __construct( Expression $left, $not = FALSE ) {
+    parent::__construct( $left );
+    if ( is_bool( $not ) ) {
+      $this->not = $not;
+    } else {
+      // TODO - Would an Illegal argument alert be useful?
+      throw Alert::alert( 'Illegal argument', 'not must be a boolean' )
+        ->culprit( 'not', $not );
+    }
+  }
 
   /**
    * Returns a Conditition object representing an IS NOT NULL check on the
@@ -50,6 +75,13 @@ class NullCondition implements Condition {
    */
   public static function isNull( $column ) {
 
+  }
+
+  /* (non-PHPdoc)
+   * @see \Substance\Core\Database\PostfixExpression::getSymbol()
+   */
+  public function getSymbol() {
+    return $this->not ? 'IS NOT NULL' : 'IS NULL';
   }
 
 }
