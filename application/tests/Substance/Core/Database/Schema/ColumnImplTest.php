e43ef7746a24c9ccaf43cc0bf9cb00868ab7a26f
Kevin Rogers <kev@orican.eu>
2015-02-23 17:41:43 +0000

    Add a table and column test.

---
+++ application/tests/Substance/Core/Database/Schema/ColumnImplTest.php
@@ -0,0 +1,38 @@
+<?php
+/* Substance - Content Management System and application framework.
+ * Copyright (C) 2015 Kevin Rogers
+ *
+ * Substance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Substance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Substance.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Substance\Core\Database\Schema;
+
+use Substance\Core\Database\SQL\AbstractSQLTest;
+use Substance\Core\Database\Schema\Types\Integer;
+
+/**
+ * Tests the column schema element.
+ */
+class ColumnImplTest extends AbstractSQLTest {
+
+  /**
+   * Tests a colummn allowing null values.
+   */
+  public function testAllowsNull() {
+    $table = new BasicTable( $this->connection, 'table' );
+    $column = new ColumnImpl( $table, 'col', new Integer() );
+    $this->assertTrue( $column->allowsNull() );
+  }
+
+}
